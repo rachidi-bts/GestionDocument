b8dde1ed259208a00c71668f6301ae9a324056df
IBTISSAM <IBTISSAM@DESKTOP-QEPOAEC>
2020-04-11 14:00:10 +0100

    test

---
+++ GestionDocument/src/main/java/com/gestion/demo/GestionDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDocumentApplication.class, args);
+	}
+
+}
