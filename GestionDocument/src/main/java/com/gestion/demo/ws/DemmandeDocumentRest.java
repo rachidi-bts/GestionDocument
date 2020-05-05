7d50fecc52a4b5403c77488b308e47bea03c85aa
lamya <lamya@DESKTOP-FBHM6H0>
2020-05-05 18:10:28 +0000

    up

--- GestionDocument/src/main/java/com/gestion/demo/ws/DemmandeDocumentRest.java
+++ GestionDocument/src/main/java/com/gestion/demo/ws/DemmandeDocumentRest.java
@@ -1,5 +1,83 @@
 package com.gestion.demo.ws;
 
-public class DemmandeDocumentRest {
+import java.util.Date;
+import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.gestion.demo.bean.DemmandeDocument;
+import com.gestion.demo.dao.DemmandeDocumentDao;
+import com.gestion.demo.service.facade.DemmandeDocumentService;
+
+ @RestController
+ @CrossOrigin(origins = { "http://localhost:4200" })
+ @RequestMapping("GestionDocument/DemmandeDocument")
+public class DemmandeDocumentRest {
+	
+	@Autowired
+	private DemmandeDocumentService demmandeDocumentService;
+	
+	@GetMapping("/Demmandeur/nom/{nom}")
+	public List<DemmandeDocument> findByDemmandeurNom(@PathVariable String nom) {
+		return demmandeDocumentService.findByDemmandeurNom(nom);
+	}
+
+
+	@GetMapping("/Demmandeur/codeApogee/{codeApogee}")
+	public List<DemmandeDocument> findByDemmandeurCodeApogee(@PathVariable String codeApogee) {
+		return demmandeDocumentService.findByDemmandeurCodeApogee(codeApogee);
+	}
+
+	@GetMapping("/Demmandeur/cne/{cne}")
+	public List<DemmandeDocument> findByDemmandeurCne(@PathVariable String cne) {
+		return demmandeDocumentService.findByDemmandeurCne(cne);
+	}
+
+	@GetMapping("/TypeDocument/libelle/{libelle}")
+	public List<DemmandeDocument> findByTypeDocumentLibelle(@PathVariable String libelle) {
+		return demmandeDocumentService.findByTypeDocumentLibelle(libelle);
+	}
+
+	@GetMapping("/EtatDemmmande/libelle/{libelle}")
+	public List<DemmandeDocument> findByEtatDemmandeLibelle(@PathVariable String libelle) {
+		return demmandeDocumentService.findByEtatDemmandeLibelle(libelle);
+	}
+
+	@GetMapping("/dateDemmande/{dateDemmande}")
+	public DemmandeDocumentDao findByDateDemmande(@PathVariable Date dateDemmande) {
+		return demmandeDocumentService.findByDateDemmande(dateDemmande);
+	}
+
+	@DeleteMapping("/EtatDemmande/libelle/{libelle}")
+	public int deleteByEtatDemmandeLibelle(@PathVariable String libelle) {
+		List<DemmandeDocument> demmandeDocuments= demmandeDocumentService.findByEtatDemmandeLibelle(libelle);
+		if (demmandeDocuments == null) {
+			return -1;
+		}else {
+			for (DemmandeDocument demmandeDocument : demmandeDocuments) {
+				demmandeDocumentService.delete(demmandeDocument);	
+			}
+			return 1;
+		
+		}
+	}
+
+   @PostMapping("/")
+	public int save(@RequestBody DemmandeDocument demmandeDocument) {
+		return demmandeDocumentService.save(demmandeDocument);
+		
+	}
+
+	@GetMapping("/")
+	public List<DemmandeDocument> findAll() {
+		return demmandeDocumentService.findAll();
+	}
+	
 }
